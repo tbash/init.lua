3f59a1cffcbfb8d9e1ee6e7dbe790563737670f6
Brandon Ashley <tb@tba.sh>
2023-12-13 19:17:31 -0500

    feat: steal more nvchad things

--- init.lua
+++ init.lua
@@ -360,15 +360,72 @@ require("lazy").setup {
   -- Autocompletion
   {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
+      {
+        -- snippet plugin
+        "L3MON4D3/LuaSnip",
+        dependencies = "rafamadriz/friendly-snippets",
+        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+        config = function(_, opts)
+          require("luasnip").config.set_config(opts)
+
+          -- vscode format
+          require("luasnip.loaders.from_vscode").lazy_load()
+          require("luasnip.loaders.from_vscode").lazy_load {
+            paths = vim.g.vscode_snippets_path or "",
+          }
+
+          -- snipmate format
+          require("luasnip.loaders.from_snipmate").load()
+          require("luasnip.loaders.from_snipmate").lazy_load {
+            paths = vim.g.snipmate_snippets_path or "",
+          }
+
+          -- lua format
+          require("luasnip.loaders.from_lua").load()
+          require("luasnip.loaders.from_lua").lazy_load {
+            paths = vim.g.lua_snippets_path or "",
+          }
+
+          vim.api.nvim_create_autocmd("InsertLeave", {
+            callback = function()
+              if
+                require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                and not require("luasnip").session.jump_active
+              then
+                require("luasnip").unlink_current()
+              end
+            end,
+          })
+        end,
+      },
+      {
+        "windwp/nvim-autopairs",
+        opts = {
+          fast_wrap = {},
+          disable_filetype = { "TelescopePrompt", "vim" },
+        },
+        config = function(_, opts)
+          require("nvim-autopairs").setup(opts)
+
+          -- setup cmp for autopairs
+          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+          require("cmp").event:on(
+            "confirm_done",
+            cmp_autopairs.on_confirm_done()
+          )
+        end,
+      },
+      {
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+      },
     },
     config = function()
-      -- Set up nvim-cmp
-      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
       local luasnip = require "luasnip"
       local cmp = require "cmp"
 
@@ -407,10 +464,10 @@ require("lazy").setup {
           { name = "nvim_lsp" },
           { name = "luasnip" },
           { name = "buffer" },
+          { name = "nvim_lua" },
+          { name = "path" },
         },
       }
-
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
   },
 
@@ -499,15 +556,6 @@ require("lazy").setup {
     lazy = false,
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
-    },
-  },
-
-  -- Pair matching characters
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts = {
-      disable_filetype = { "TelescopePrompt", "vim" },
     },
   },
 
